0680ea26fac7287f53c347e57ff4ceb6ec27e139
EmaaZmaker <emaazbinammar@gmail.com>
2025-07-28 19:00:31 +0400

    v

---
+++ apy.py
@@ -0,0 +1,10 @@
+class dog:
+    def __init__(self,name,breed):
+        self.name=name
+        self.breed=breed
+ob1=dog("Alam", "Pitbull")
+ob2=dog("Spike","Fluffy")
+print(f"the breed of Alam {ob1.breed}")
+print(f"The breed of Spike is {ob2.breed}")
+
+        
