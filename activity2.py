70b7e13311d3079ec0865e59146815c21693d479
EmaaZmaker <emaazbinammar@gmail.com>
2025-07-28 11:43:37 +0400

    car

---
+++ activity2.py
@@ -0,0 +1,9 @@
+class car:
+    def __init__(self,max_speed,mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+ob=car(350,50)
+print("max speed is:",ob.max_speed)
+print("the mileage for this car is:",ob.mileage)
+        
+        
